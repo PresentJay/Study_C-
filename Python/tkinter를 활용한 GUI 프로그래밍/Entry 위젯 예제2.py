08fbf08434f18a2e33d5c963e31b00a593e334ec
PresentJay <presentj94@gmail.com>
2019-05-31 02:11:37 +0900

    python tkinter widget practice

---
+++ Python/tkinter를 활용한 GUI 프로그래밍/Entry 위젯 예제2.py
@@ -0,0 +1,16 @@
+from tkinter import *
+top = Tk()
+
+def showName():
+    print("User Name:", en.get())
+
+lb = Label(top, text="User Name").grid(row=0)
+en = Entry(top)
+en.grid(row=0, column=1)
+
+bt1 = Button(top, text='Show', command=showName)
+bt1.grid(row=1, column=0)
+bt2 = Button(top, text="Quit", command=top.quit)
+bt2.grid(row=1, column=1)
+
+top.mainloop()
