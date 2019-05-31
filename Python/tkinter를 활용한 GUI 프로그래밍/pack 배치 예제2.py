2606cadcb1601e3ab96b88a8206b78ec8c749f3f
PresentJay <presentj94@gmail.com>
2019-06-01 03:13:13 +0900

    python tkinter pack with side, pad, fill

---
+++ Python/tkinter를 활용한 GUI 프로그래밍/pack 배치 예제2.py
@@ -0,0 +1,8 @@
+from tkinter import *
+w= Tk()
+
+Button(w, text='python 언어').pack(fill='x')
+Button(w, text='C++ 언어').pack(fill='x')
+Button(w, text='Visual Basic 언어').pack(fill='x')
+
+w.mainloop()
---
+++ Python/tkinter를 활용한 GUI 프로그래밍/pack 배치 예제4(side).py
@@ -0,0 +1,8 @@
+from tkinter import *
+w= Tk()
+
+Button(w, text='python 언어').pack(fill='x', padx=10, side=LEFT)
+Button(w, text='C++ 언어').pack(fill='x', padx=10, pady=5, side=LEFT)
+Button(w, text='Visual Basic 언어').pack(fill='x', padx=10, side=LEFT)
+
+w.mainloop()
