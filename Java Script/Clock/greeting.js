4ac90dc2d498194e21f6251d9d70be7cf21338b4
presentJay <presentj94@gmail.com>
2020-01-03 02:19:03 +0900

    vanila javascript / localstorage / prac2

--- Java Script/Clock/greeting.js
+++ Java Script/Clock/greeting.js
@@ -5,18 +5,34 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerHTML = `Hello ${text}`;
 }
 
-
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
         // he or she is not
-
+        askForName();
     } else {
         // he or she is
         paintGreeting(currentUser);
